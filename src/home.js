5c199ed8edd8af4f61754ae287482d5a057dff71
Fredrick Uche <fredrickcuche@gmail.com>
2023-06-16 13:16:40 +0100

    ADDED blog list

--- src/home.js
+++ src/home.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
+
 const Home = () => {
+   const [blogs, setBlogs] = useState([
+    {title: 'My new website', body: 'lorem ispum..', author: 'mario', id: 1},
+    {title: 'welcome party', body: 'lorem ispum..', author: 'yoshi', id: 2}, 
+    {title: 'new website', body: 'lorem ispum..', author: 'game', id: 3}
+   ]);
+    
+     
     return (  
         <div className="home">
-            <h1>Home page</h1>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+
+            ))}
+        
         </div>
     );
 }
