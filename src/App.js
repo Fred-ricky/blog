62e5d74c24e4f8fc19480f4e2a6f44b92df48187
Fredrick Uche <fredrickcuche@gmail.com>
2023-06-20 13:24:51 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import Navbar from './navbar';
 import Home from './home';
-
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
 function App() {
 
 
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <switch>
+          <Route path="/">
+            <Home />
+
+          </Route>
+        </switch>
       </div>
     </div>
+    </Router>
   );
 }
 
--- src/home.js
+++ src/home.js
@@ -1,39 +1,18 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
-
+import useFetch from "./useFetch";
 
 const Home = () => {
-   const [blogs, setBlogs] = useState(null);
-   const [isPending, setISPending] = useState(true);
-   const [error, setError] = useState(null);
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
    
-   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-        .then(res => {
-            if (res.ok) {
-                throw Error('COULD NOT FETCH THE DATA');
-            }
-          return  res.json();
-        })
-        .then(data => {
-            console.log(data);
-            setBlogs(data);
-            setISPending(false);
-            setError(null);
-        })
-        .catch(err => {
-            setISPending(false);
-            setError(err.message);
-    });
-   }, []);
+   
     
     
 return (  
     <div className="home">
         { error && <div>{ error }</div>}
         { isPending && <div>loading ...</div>}
-        {blogs && <BlogList  blogs={blogs} title='All Blogs!' />}
+        { blogs && <BlogList  blogs={blogs} title='All Blogs!' />}
        
     </div>
     );
