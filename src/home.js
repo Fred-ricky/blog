148165b723641655e1b6500e0f0d23715eba45c7
Fredrick Uche <fredrickcuche@gmail.com>
2023-06-20 11:49:53 +0100

    handled error messages

--- src/home.js
+++ src/home.js
@@ -5,26 +5,33 @@ import BlogList from "./BlogList";
 const Home = () => {
    const [blogs, setBlogs] = useState(null);
    const [isPending, setISPending] = useState(true);
+   const [error, setError] = useState(null);
 
    
    useEffect(() => {
     fetch('http://localhost:8000/blogs')
         .then(res => {
-          return  res.json();})
+            if (res.ok) {
+                throw Error('COULD NOT FETCH THE DATA');
+            }
+          return  res.json();
+        })
         .then(data => {
             console.log(data);
             setBlogs(data);
             setISPending(false);
+            setError(null);
         })
         .catch(err => {
-        console.log(err.message);
+            setISPending(false);
+            setError(err.message);
     });
    }, []);
     
     
 return (  
     <div className="home">
-        
+        { error && <div>{ error }</div>}
         { isPending && <div>loading ...</div>}
         {blogs && <BlogList  blogs={blogs} title='All Blogs!' />}
        
